ad07e76ab303f95a1a09304ba8629d4d8f813d7d
Chandrasekar Gokulanathan <g.chandrasekar1993@gmail.com>
2018-07-09 14:09:00 +0530

    Support for section list

--- src/SearchableSectionList.js
+++ src/SearchableSectionList.js
@@ -6,17 +6,37 @@ export default class SearchableSectionList extends Component {
   static propTypes = {
     sections: PropTypes.array.isRequired,
     searchTerm: PropTypes.string.isRequired,
-    searchAttribute: PropTypes.string.isRequired
+    searchAttribute: PropTypes.string.isRequired,
+    ignoreCase: PropTypes.bool
+  };
+
+  static defaultProps = {
+    searchAttribute: "",
+    ignoreCase: true
   };
 
   render() {
+    const { sections, searchAttribute, searchTerm, ignoreCase } = this.props;
     return (
       <SectionList
         {...this.props}
-        sections={this.props.sections.filter(tempData => {
-          tempData = tempData.data;
-          return tempData.some(a => {
-            return a.includes(this.props.searchTerm);
+        sections={sections.filter(sectionData => {
+          sectionData = sectionData.data;
+          let searchData = sectionData;
+
+          return searchData.some(data => {
+            let searchDataItem = searchAttribute
+              ? searchAttribute
+                  .split(".")
+                  .reduce((prevVal, currVal) => prevVal[currVal], data)
+              : data;
+            if (ignoreCase) {
+              return searchDataItem
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase());
+            } else {
+              return searchDataItem.includes(searchTerm);
+            }
           });
         })}
       />
